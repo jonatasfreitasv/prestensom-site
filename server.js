7abfe16893337cbe1421f16132280fdfeaa9f3a6
Jonatas Freitas <jonatas.vargas@lojasrenner.com.br>
2018-08-28 21:31:48 -0300

    Template

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static('public'));
+app.use(express.static('files'));
+
+app.use('/', express.static(__dirname + '/app'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
